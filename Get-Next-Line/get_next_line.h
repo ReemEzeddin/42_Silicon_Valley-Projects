7479de398ba42cb0c7b57e99a4e35e98851af88c
ReemEzeddin <Reem_ezzidin@hotmail.com>
2020-12-15 11:24:24 -0600

    Latest Update

--- Get-Next-Line/get_next_line.h
+++ Get-Next-Line/get_next_line.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: root <marvin@42.fr>                        +#+  +:+       +#+        */
+/*   By: reezeddi <marvin@42.f>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/12/03 13:57:17 by root              #+#    #+#             */
-/*   Updated: 2020/12/04 13:59:41 by reezeddi         ###   ########.fr       */
+/*   Created: 2020/12/13 15:37:24 by reezeddi          #+#    #+#             */
+/*   Updated: 2020/12/15 10:54:31 by reezeddi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,21 @@
 # define GET_NEXT_LINE_H
 
 # include <stdlib.h>
-# include <sys/types.h>
-# include <sys/uio.h>
+# include <stdio.h>
 # include <unistd.h>
-# include <string.h>
 
 # define BUFF_SIZE 42
 
-typedef struct		s_file_reader
-{
-	char	*buffer;
-	char	*cur_pos;
-}					t_file;
-
-int					get_next_line(const int fd, char **line);
-void				*ft_memalloc(size_t size);
-void				*ft_memset(void *dst, int c, size_t n);
-char				*ft_strcat(char *dst, const char *src);
-char				*ft_strchr(const char *str, int c);
-char				*ft_strdup(const char *str);
-char				*ft_strjoin(char const *s1, char const *s2);
-size_t				ft_strlen(const char *str);
-char				*ft_strnew(size_t size);
-void				ft_del(char *str);
+int		get_next_line(int fd, char **line);
+void	*ft_memalloc(size_t size);
+void	*ft_memset(void *dst, int c, size_t n);
+char	*ft_strchr(const char *str, int c);
+char	*ft_strdup(const char *str);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlen(const char *str);
+char	*ft_strnew(size_t size);
+int		ft_del(char *str);
+int		get_result(ssize_t x, char **newline, char **tmp, char **line);
 
 #endif
